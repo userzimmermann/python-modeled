6dae9e161eef41995c7cb1fff69c2b5acba19e73
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-05-11 14:43:07 +0000

    Little doc string fix.

--- modeled/member/tuple.py
+++ modeled/member/tuple.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with python-modeled.  If not, see <http://www.gnu.org/licenses/>.
 
-"""modeled.member
+"""modeled.member.tuple
 
 .. moduleauthor:: Stefan Zimmermann <zimmermann.code@gmail.com>
 """
