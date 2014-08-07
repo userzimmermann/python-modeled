adea2d4195d46bbf46c23ffd6f712d3a33f898eb
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-08-07 19:43:01 +0000

    setup: Removed scons.

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
-import sys
-from subprocess import call
-
-
 # Run the zetup script (reading config from zetuprc):
 exec(open('__init__.py').read())
-
-if 'sdist' in sys.argv:
-    status = call('scons')
-    if status:
-        sys.exit(status)
 
 
 zetup(
